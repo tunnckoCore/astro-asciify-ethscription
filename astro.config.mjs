28c955bb5b43ade58a3a93d9b75de0f9f3a77480
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2024-01-18 12:52:27 +0200

    chore: cleanup
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
@@ -8,5 +8,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
 });
